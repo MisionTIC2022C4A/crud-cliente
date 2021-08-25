017005f68c3c235e4b136dc6b9c8984379f100fb
Gabriel Marquez <marquez122@outlook.com>
2021-08-25 09:44:22 -0500

    Returns Patient id and idType on delete

--- src/main/java/com/misiontic/patient_ms/controllers/PatientController.java
+++ src/main/java/com/misiontic/patient_ms/controllers/PatientController.java
@@ -18,7 +18,7 @@ public class PatientController {
     }
 
     @GetMapping("/patient/{numeroIdentificacion}/{tipoIdentificacion}")
-    public Patient getPatient(@PathVariable String numeroIdentificacion,@PathVariable String tipoIdentificacion){
+    public Patient getPatient(@PathVariable String numeroIdentificacion, @PathVariable String tipoIdentificacion){
         return patientRepository.findByNumeroIdentificacionAndTipoIdentificacion(
                 numeroIdentificacion, tipoIdentificacion
         ).orElseThrow(() -> new PatientNotFoundException("No se encontro una cuenta con el numeroIdentificacion " +
@@ -26,12 +26,15 @@ public class PatientController {
     }
     @Transactional
     @DeleteMapping("/patient/{numeroIdentificacion}/{tipoIdentificacion}")
-    public void deletePatient(@PathVariable String numeroIdentificacion,@PathVariable String tipoIdentificacion){
+    public String deletePatient(@PathVariable String numeroIdentificacion,@PathVariable String tipoIdentificacion){
         patientRepository.deleteByNumeroIdentificacionAndTipoIdentificacion(
                 numeroIdentificacion, tipoIdentificacion);
+        return "Se ha eliminado el paciente con el numeroIdentificacion " +
+                "y el tipoIdentificacion: " + numeroIdentificacion + ", " + tipoIdentificacion;
     }
     @PostMapping("/patient")
     public Patient newPatient(@RequestBody Patient newPatient){
+        //TODO check if Patient already exist
         return patientRepository.save(newPatient);
     }
 }
